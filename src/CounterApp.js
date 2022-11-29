3f377e5c4f631c7d2d1319f6bda84084c7be44c9
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2022-11-28 22:21:06 -0500

    useState Hook

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
+import {useState} from 'react';
 
 const CounterApp = ({value}) => {
 
     //handleAdd
+    const [counter, setCounter ] = useState(0);
+
+
 
     const handleAdd = (e) => {
-        console.log(e); 
+        setCounter(counter + 1);
     }
 
     return (
         <>
            <h1> CounterApp </h1>
-           <h2> {value} </h2>
+           <h2> {counter} </h2>
            <button onClick={handleAdd}>Sumar +1</button>    
         </>
     )
