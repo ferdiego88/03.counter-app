d15fdb7c7b806b3f91a1c8c75aacfe5f38a8c3d6
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2022-11-28 21:52:00 -0500

    tarea rafcp

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import PrimeraAPP from "./PrimerApp";
 import './index.css';
+import CounterApp from "./CounterApp";
 
 //const saludo = <h1>Hola Mundo</h1>;
 
 const divRoot = document.querySelector('#root');
 
 const root = ReactDOM.createRoot(divRoot);
-root.render(<PrimeraAPP saludo="Hola mundo"/>);
+// root.render(<PrimeraAPP saludo="Hola mundo"/>);
+
+root.render(<CounterApp value = {12092022} />)
