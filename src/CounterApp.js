6ee4d6e5dc3f0f8ee848ffca437159cbc9201e01
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2022-12-30 22:37:25 -0500

    handleReset y Substtract

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,23 +1,35 @@
 import PropTypes from "prop-types";
 import {useState} from 'react';
 
-const CounterApp = ({value}) => {
+const CounterApp = ({value = 10}) => {
 
     //handleAdd
-    const [counter, setCounter ] = useState(0);
+    const [counter, setCounter ] = useState(value); //Retorna un arreglo
 
 
 
     const handleAdd = (e) => {
+        // console.log(e);
         setCounter(counter + 1);
         // setCounter( (c) => c + 1 );
     }
+
+    const handleSubstract = (e) => {
+        setCounter(counter -1);
+    }
+
+    const handleReset = (e) => {
+        setCounter(0);
+    }
+
 
     return (
         <>
            <h1> CounterApp </h1>
            <h2> {counter} </h2>
-           <button onClick={handleAdd}>Sumar +1</button>    
+           <button onClick={handleAdd}>Sumar +1</button>   
+           <button onClick={handleReset}>Reset</button>   
+           <button onClick={handleSubstract}>Restar -1</button>   
         </>
     )
 }
