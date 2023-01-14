0c43dda01c369c6c181de106872cd850245c0b8b
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2023-01-14 18:59:36 -0500

    pruebas con Enzyme

--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,14 +1,48 @@
 import { render,screen } from "@testing-library/react";
 import PrimeraAPP from "../PrimerApp";
+import { shallow }from 'Enzyme'
 
 describe('Pruebas en <PrimeraApp />', () => {
 
-    test('debe de mostrar el mensaje "Hola, soy Goku"', () => {
 
-        const saludo = "Hola, soy Goku";
+    //PRUEBAS CON JEST
 
-        render(<PrimeraAPP saludo= {saludo}/>);
-        expect( screen.getByText(saludo)).toBeInTheDocument();
+    // test('debe de mostrar el mensaje "Hola, soy Goku"', () => {
+
+    //     const saludo = "Hola, soy Goku";
+
+    //     render(<PrimeraAPP saludo= {saludo}/>);
+    //     expect( screen.getByText(saludo)).toBeInTheDocument();
         
+    // })
+
+    //PRUEBA CON ENZYME
+
+    test('debe de mostrar </PrimeraApp> correctamente', () => { 
+
+        const saludo = 'Hola, soy Goku';
+
+        const wrapper = shallow(<PrimeraAPP saludo={saludo} /> );
+
+        expect(wrapper).toMatchSnapshot();
     })
+
+    test('debe de mostrar el subtítulo enviado por props', () => { 
+
+        
+        const saludo = 'Hola, soy Goku';
+        const subtitulo = 'Soy un subtitulo';
+       
+        const wrapper = shallow(
+        <PrimeraAPP 
+            saludo={saludo} 
+            subtitulo={subtitulo} 
+        /> 
+        );
+        
+        const textoParrafo = wrapper.find('p').text();
+        console.log(textoParrafo);
+
+        expect(textoParrafo).toBe(subtitulo);
+     })
 })
