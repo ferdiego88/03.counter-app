f0ac44353aa1495dfaa5bcc9a7567379949b7b28
Fernando Diego Santillan Varas <diego.santillan@pragma.com.co>
2023-01-31 21:40:39 -0500

    prueba con boton reset

--- src/tests/base/CounterApp.test.js
+++ src/tests/base/CounterApp.test.js
@@ -47,4 +47,19 @@ describe('Pruebas en <CounterApp />',() => {
     
             expect(counterText).toBe('9');
         })
+
+        test('debe de colocar el valor por defecto del boton btn reset', () => { 
+            
+
+            const wrapper = shallow( <CounterApp value={105}/>);
+
+            wrapper.find('button').at(0).simulate('click');
+            wrapper.find('button').at(0).simulate('click');
+            wrapper.find('button').at(1).simulate('click');
+
+            const counterText = wrapper.find('h2').text().trim();
+
+            expect(counterText).toBe('105');
+
+         })
 })
